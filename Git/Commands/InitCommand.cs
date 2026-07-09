0ddfb4de09206982e7517b454081c8170c6f3728
Arushi Gupta <arushig223@gmail.com>
2026-07-09 18:05:38 +0530

    Add: implement repository initialization

--- Git/Commands/InitCommand.cs
+++ Git/Commands/InitCommand.cs
@@ -1,9 +1,22 @@
+using Git.Repository;
+
 namespace Git.Commands;
 
 public class InitCommand
 {
+    GitRepository _repository = new GitRepository();
     public void Execute()
     {
-        Console.WriteLine("Initializing repository...");
+        string path = Directory.GetCurrentDirectory();
+        try
+        {
+            _repository.Initialize(path);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+        }
+        
+        
     }
 }
---
+++ Git/Repository/GitRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git.Repository
+{
+    internal class GitRepository
+    {
+       
+        public void Initialize(string repositoryPath)
+        {
+            string gitPath = Path.Combine(repositoryPath, ".git");
+            if (Directory.Exists(gitPath))
+            {
+                Console.WriteLine($"Reinitialized existing Git repository in {repositoryPath}");
+            } else
+            {
+                Console.WriteLine($"Initialized empty Git repository in {repositoryPath}");
+            }
+            CreateDirectoryStructure(gitPath);
+            CreateHeadFile(gitPath);
+            CreateConfigFile(gitPath);
+        }
+
+        private void CreateDirectoryStructure(string gitPath)
+        {
+            string objDir = Path.Combine(gitPath, "objects");
+            string headDir = Path.Combine(gitPath, "refs", "heads");
+            Directory.CreateDirectory(gitPath);
+            Directory.CreateDirectory(objDir);
+            Directory.CreateDirectory(headDir);   
+        }
+
+        private void CreateHeadFile(string gitPath)
+        {
+            string headPath = Path.Combine(gitPath, "HEAD");
+            string content = "ref: refs/heads/main";
+            File.WriteAllText(headPath, content);
+        }
+
+        private void CreateConfigFile(string gitPath)
+        {
+            string configPath = Path.Combine(gitPath, "config");
+            string content = "[core]\r\n    repositoryformatversion = 0";
+            File.WriteAllText(configPath, content);
+        }
+    }
+
+}
